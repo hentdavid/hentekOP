18c782b635bb3d2ae1b57f7306811a23100c7e8b
hentdavid <hentek.d@sosceskybrod.cz>
2024-05-20 10:09:23 +0200

    Revert "Add files via upload"
    This reverts commit 2757dcacb09779b470fb7d37c816dda06f7481a4.

--- Box.java
+++
@@ -1,9 +0,0 @@
-package org.example.logic;
-
-public class Box extends Entity {
-    public boolean isBroken;
-    public int timer;
-    public Box (int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
-}
